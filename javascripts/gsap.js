ccdfada8011c64ac073a7956855371fdcc50371e
Nathraktim <111349530+Nathraktim@users.noreply.github.com>
2024-07-14 12:51:23 +0530

    Update gsap.js

--- javascripts/gsap.js
+++ javascripts/gsap.js
@@ -5,4 +5,46 @@ gsap.from('.main',
             y: 30,
             opacity: 0
         });
+
+        gsap.from('.two',
+            {
+                duration: 3,
+                opacity: 0,
+                scrollTrigger: {
+                    trigger: '.two',
+                    start: 'top 90%',
+                    end: 'bottom 20%',
+                    scrub: 3,
+                    pinned: true,
+                    markers: true
+                }
+            });
+            gsap.from('.values',
+                {
+                    duration: 3,
+                    opacity: 0,
+                    y: 50,
+                    scrollTrigger: {
+                        trigger: '.values',
+                        start: 'top 90%',
+                        end: 'bottom 20%',
+                        scrub: 3,
+                        pinned: true,
+                        markers: true
+                    }
+                });
+                gsap.from('.dedicatedIntro',
+                    {
+                        duration: 3,
+                        opacity: 0,
+                        y: 50,
+                        scrollTrigger: {
+                            trigger: '.dedicatedIntro',
+                            start: 'top 90%',
+                            end: 'bottom 20%',
+                            scrub: 3,
+                            pinned: true,
+                            markers: true
+                        }
+                    });
 });
